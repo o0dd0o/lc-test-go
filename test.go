c6f3023fb8bd1c3b8600c2f27ba014a95bcb82a0
dengbiao <1269025405@qq.com>
2020-07-28 02:21:33 +0800

    1.first

--- test.go
+++ test.go
@@ -4,20 +4,40 @@ import (
 	"fmt"
 )
 
-func count(s string, t string) int {
-	return 3
+func count(s string, t string) bool {
+	s_m, t_m := s2i(s), s2i(t)
+	lenS := len(s_m)
+	for i := 0; i < lenS; i++ {
+		if s_m[i] != t_m[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func s2i(s string) []int {
-	var a []int
-	fmt.Printf("%c\n", s[0])
+	lenS := len(s)
+	a := make([]int, lenS)
+
+	setS := make(map[uint8]int, lenS)
+
+	for i := 0; i < lenS; i++ {
+		if setS[s[i]] == 0 {
+			setS[s[i]] = i
+		}
+	}
+
+	for i := 0; i < lenS; i++ {
+		a[i] = setS[s[i]]
+	}
+
 	return a
 }
 
 func main() {
 	s := "add"
-	t := "egg"
-	s2i(s)
+	t := "ege"
+
 	d := count(s, t)
 	fmt.Println(d)
 }
