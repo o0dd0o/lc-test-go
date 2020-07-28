fbefdf91ac12f06037b9ad6d4712871c7176c692
o0dd0o <1243421abc>
2020-07-28 18:51:06 +0800

    1.206

--- 206.go
+++ 206.go
@@ -8,11 +8,36 @@
 
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func reverseList(head *ListNode) *ListNode {
-	return head
+	top, tm := &ListNode{}, &ListNode{}
+	for head != nil {
+		tm = head
+		head = head.Next
+		tm.Next = top.Next
+		top.Next = tm
+	}
+	return top.Next
+}
+
+func main() {
+	//1->2->3->4->5->NULL
+	//1<-t 2->3->4->5->NULL
+	//1<-2<-t 3->4->5->NULL
+	a := &ListNode{Val: 5}
+	b := &ListNode{Val: 4, Next: a}
+	c := &ListNode{Val: 3, Next: b}
+	d := &ListNode{Val: 2, Next: c}
+	f := &ListNode{Val: 1, Next: d}
+	f_ := reverseList(f)
+	for f_ != nil {
+		fmt.Printf("%d\n", f_.Val)
+		f_ = f_.Next
+	}
 }
