a642e6020220ab40e87c97b266fa3e8bd3fbc7aa
o0dd0o <1243421abc>
2020-07-29 09:54:20 +0800

    1.217

--- 217.go
+++ 217.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
 给定一个整数数组，判断是否存在重复元素。
 
@@ -23,9 +25,22 @@ package main
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 func containsDuplicate(nums []int) bool {
-	return true
+	numsMap := make(map[int]bool)
+	numsLen := len(nums)
+	for i := 0; i < numsLen; i++ {
+		if numsMap[nums[i]] {
+			return true
+		}
+		numsMap[nums[i]] = true
+	}
+	return false
 }
 
 func main() {
-
+	nums := []int{1, 2, 3, 1}
+	nums2 := []int{1, 2, 3, 4}
+	nums3 := []int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}
+	fmt.Printf("%v\n", containsDuplicate(nums))
+	fmt.Printf("%v\n", containsDuplicate(nums2))
+	fmt.Printf("%v\n", containsDuplicate(nums3))
 }
