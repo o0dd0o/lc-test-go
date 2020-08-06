8d3eb3cdcabd4273af1a39150f8b407aefeebd9d
o0dd0o <1243421abc>
2020-08-06 10:55:39 +0800

    1.235

--- 234/234.go
+++ 234/234.go
@@ -31,41 +31,41 @@ type ListNode struct {
 }
 
 func isPalindrome(head *ListNode) bool {
-	lenL := 0
-	for top := head; top != nil; top = top.Next {
-		lenL += 1
+	if head == nil || head.Next == nil {
+		return true
 	}
 
-	if lenL == 0 {
-		return false
+	slow, fast, top, tm := head, head, &ListNode{}, head
+
+	for fast != nil {
+		if fast.Next == nil {
+			slow = slow.Next
+			break
+		}
+		fast = fast.Next.Next
+		tm = slow
+		slow = slow.Next
+		tm.Next = top
+		top = tm
 	}
 
-	slow, fast, c := head, head, 0
-	/**
-		1 -> 2 -> 2 -> 1
-	         s	  f
-		1 -> 2 -> 3 -> 2 -> 1
-			 s    f
-	              s        f
-		1 -> 2 -> 3 -> 3 -> 2 -> 1
-	         s    f
-	              s         f
-		1 -> 2 -> 3 -> 4 -> 3 -> 2 -> 1
-			 s    f
-	              s         f
-	                   s              f
-	*/
-	for true {
-		break
+	for ; slow != nil; slow, top = slow.Next, top.Next {
+		if slow.Val != top.Val {
+			return false
+		}
 	}
 
-	return false
+	return true
 }
-
+func printNode(head *ListNode) {
+	print(head.Val, " ")
+	if head.Next != nil {
+		printNode(head.Next)
+	}
+}
 func main() {
 	a := &ListNode{Val: 1}
 	b := &ListNode{Val: 2, Next: a}
-	c := &ListNode{Val: 2, Next: b}
-	d := &ListNode{Val: 1, Next: c}
-	println(isPalindrome(d))
+	c := &ListNode{Val: 1, Next: b}
+	println(isPalindrome(c))
 }
